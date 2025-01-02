1340f571b0b962865eaa5a463fe6c14143347e49
che2ese <141838530+che2ese@users.noreply.github.com>
2025-01-03 00:01:34 +0900

    Scene5 repair

--- Assets/Scripts/S5/S5Manager.cs
+++ Assets/Scripts/S5/S5Manager.cs
@@ -54,6 +54,16 @@ public class S5Manager : MonoBehaviourPun
     public AudioClip mirrorSound;
     public AudioClip bridgeSound;
 
+    TalkManager tm;
+    public GameObject boss;
+    public GameObject missionPanel;
+    public TypeEffect missionTmp;
+    private bool isTalking = true;
+
+    void Awake()
+    {
+        tm = FindObjectOfType<TalkManager>();
+    }
     void Start()
     {
         if (PhotonNetwork.IsMasterClient)
@@ -66,6 +76,8 @@ public class S5Manager : MonoBehaviourPun
 
             // 모든 클라이언트에 정답을 공유
             photonView.RPC("SetCorrectAnswer", RpcTarget.All, correctAnswer, correctAnswer2);
+
+            StartCoroutine(ShowMission());
         }
     }
 
@@ -77,8 +89,24 @@ public class S5Manager : MonoBehaviourPun
             EndZone.SetActive(true);
             Debug.Log("CAN END!!!");
         }
+        if (missionPanel.activeSelf)
+        {
+            boss.SetActive(false);
+        }
+        else
+            boss.SetActive(true);
     }
 
+    IEnumerator ShowMission()
+    {
+        if (isTalking)
+        {
+            missionTmp.SetMsg(tm.S5Text[0]);
+            yield return new WaitForSeconds(tm.S5Text[0].Length / missionTmp.CharPerSeconds + 2f); // 대사 길이만큼 기다림
+            isTalking = false;
+            missionPanel.SetActive(false);
+        }
+    }
     public void EndCandle()
     {
         photonView.RPC("EndCandleRPC", RpcTarget.All);
